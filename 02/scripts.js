d7e0c41adf224f9630d42dc8f7d5f5403edf51db
ConnorChristensen99 <connor.christensen@uvu.edu>
2023-02-01 14:35:40 -0700

    Set up pages for next assignment

---
+++ 02/scripts.js
@@ -0,0 +1,60 @@
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+    speed: 2000,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+
+    autoplay: {
+        delay: 5000
+    },
+})
+
+function toggleMenu() {
+  document.getElementById("hamburgerBtn").classList.toggle('open');
+  document.getElementById('primaryNav').classList.toggle('open')
+  document.getElementById('searchBar').classList.toggle('invisible')
+}
+
+var x = document.getElementById('hamburgerBtn');
+
+x.onclick = toggleMenu;
+
+let parent = document.getElementById('parent')
+let subList = document.getElementById('subList')
+subList.classList.add('invisible')
+
+parent.addEventListener('mouseover', () => {
+  if (subList.classList.contains('invisible')) {
+    subList.classList.remove('invisible')
+  }else
+  subList.classList.add('invisible')
+  subList.style.display = "flex"
+  subList.style.flexDirection = "column"
+})
+parent.addEventListener('mouseout', () => {
+  if (subList.classList.contains('invisible')) {
+    subList.classList.remove('invisible')
+  }else
+  subList.classList.add('invisible')
+  subList.style.display = "none"
+})
+subList.addEventListener('mouseover', () => {
+  if (subList.classList.contains('invisible')) {
+    subList.classList.remove('invisible')
+  }else
+  subList.classList.add('invisible')
+  subList.style.display = "flex"
+  subList.style.flexDirection = "column"
+})
+subList.addEventListener('mouseout', () => {
+  if (subList.classList.contains('invisible')) {
+    subList.classList.remove('invisible')
+  }else
+  subList.classList.add('invisible')
+  subList.style.display = "none"
+})
