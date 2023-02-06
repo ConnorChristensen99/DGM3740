b1e4465b1ed900fdcb8e47472b9b6f3a39b4036e
Connor Christensen <connor.christensen@uvu.edu>
2023-02-06 15:04:36 -0700

    Finished cruises page except for adding images of the cruises

--- 02/cruises.php
+++ 02/cruises.php
@@ -8,6 +8,7 @@
 
   <div class="indivCruises">
     <a href="#"><img class='cruisesImage' src = "#" alt="Cruise 1"></a>
+    <div class='cruiseInfo'>
     <h3>Australia & New Zeland</h3>
     <h4>Cruises from $899!</h4>
     <p>On the other side of the world, Australia and New
@@ -16,33 +17,36 @@
     discounts to cruise the South Pacific. Sail to New
     Zealand from Sydney, Australia on a 12-day cruise
     from just $888 per person plus taxes.</p>
-    <button type ="submit" id="learnMoreCruises">Learn More</button>
+    <a href="#"><button class="cruiseButtons" type ="button" id="learnMoreCruises">Learn More</button></a>
+  </div>
+</div>
+
+  <div class="indivCruises">
+    <a href="#"><img class='cruisesImage' src = "#" alt="Cruise 2"></a>
+    <div class='cruiseInfo'>
+    <h3>Bahamas</h3>
+    <h4>Cruises from $499!</h4>
+    <p>On a Bahamas cruise, you'll experience a variety of 
+      exciting beach activities and in-depth shore excursions. 
+      Enjoy relaxing days lounging on white sand beaches and 
+      swimming in clear turquoise waters. Go snorkeling in the 
+      beautiful Bahamian coral reefs with extraordinary sea creatures.</p>
+      <a href="#"><button class="cruiseButtons" type ="button" id="learnMoreCruises">Learn More</button></a>
+  </div>
   </div>
 
   <div class="indivCruises">
-    <a href="#"><img class='cruisesImage' src = "#" alt="Cruise 1"></a>
-    <h3>Australia & New Zeland</h3>
-    <h4>Cruises from $899!</h4>
-    <p>On the other side of the world, Australia and New
-    Zealand are full of wonders to behold! Now, more
-    than ever before, you can get phenomenal
-    discounts to cruise the South Pacific. Sail to New
-    Zealand from Sydney, Australia on a 12-day cruise
-    from just $888 per person plus taxes.</p>
-    <button type ="submit" id="learnMoreCruises">Learn More</button>
+    <a href="#"><img class='cruisesImage' src = "#" alt="Cruise 3"></a>
+    <div class='cruiseInfo'>
+    <h3>Luxury Cruises to Mexico</h3>
+    <h4>Cruises from $1299!</h4>
+    <p>There are endless treasures waiting to be discovered aboard 
+      one of our luxury cruises to Mexico. This enchanting destination
+       has everything you've ever dreamed of for an unforgettable vacation: 
+        white sand beaches, ancient ruins, mouthwatering cuisine, and breathtaking 
+        sunsets.</p>
+    <a href="#"><button class="cruiseButtons" type ="button" id="learnMoreCruises">Learn More</button></a>
   </div>
-
-  <div class="indivCruises">
-    <a href="#"><img class='cruisesImage' src = "#" alt="Cruise 1"></a>
-    <h3>Australia & New Zeland</h3>
-    <h4>Cruises from $899!</h4>
-    <p>On the other side of the world, Australia and New
-    Zealand are full of wonders to behold! Now, more
-    than ever before, you can get phenomenal
-    discounts to cruise the South Pacific. Sail to New
-    Zealand from Sydney, Australia on a 12-day cruise
-    from just $888 per person plus taxes.</p>
-    <button type ="submit" id="learnMoreCruises">Learn More</button>
   </div>
 
   </div>
